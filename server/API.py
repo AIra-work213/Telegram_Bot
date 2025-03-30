87aa47f0457fa64a3bcf51658c49f434544513ac
AIra <AIra.work213@gmail.com>
2025-03-30 20:08:20 +0700

    Добавлена обработка ответа от модели

--- server/API.py
+++ server/API.py
@@ -3,6 +3,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def func():
-    pass
+@app.post('/')
+def func(x):
+    y =  model.model(x.unsqueeze(0)).squeeze(0)
+    categories = model.description
+    response = [[categories[i], 0] for i in range(len(categories))]
+    for i in range(len(y)):
+        response[i][1]=y[i]
+    response = sorted(response, key=lambda x: x[1], reverse=True)
+    return response
