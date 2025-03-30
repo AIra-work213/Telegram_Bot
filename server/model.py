8bd30af6164ec8573303fda979306d8889b0e675
AIra <AIra.work213@gmail.com>
2025-03-30 20:09:04 +0700

    Прописана последняя актуальная версия модели

--- server/model.py
+++ server/model.py
@@ -1,11 +1,45 @@
 import torch
 import torchvision.transforms.v2 as tfs_v2
 
+transforms = tfs_v2.Compose([
+    tfs_v2.Resize((150, 150)),
+    tfs_v2.ToImage(),
+    tfs_v2.ToDtype(dtype=torch.float32)
+])
+
 class Model(torch.nn.Module):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.layers = torch.nn.Sequential(
+            torch.nn.Conv2d(3, 20, 3, 1, 1, bias=False),
+            torch.nn.MaxPool2d(3),
+            torch.nn.ReLU(True),
+            torch.nn.BatchNorm2d(20),
+            torch.nn.Conv2d(20, 50, 3, 1, 1, bias=True),
+            torch.nn.Conv2d(50, 200, 3, 1, 1, bias=False),
+            torch.nn.MaxPool2d(10),
+            torch.nn.ReLU(True),
+            torch.nn.BatchNorm2d(200),
+            torch.nn.Flatten(),
+            torch.nn.Linear(5000, 1000),
+            torch.nn.ReLU(True),
+            torch.nn.Linear(1000, 6),
+            torch.nn.Sigmoid()
+        )
+
     def forward(self, x):
-        return x
+        return self.layers(x)
 
 
-copy = torch.load('weights.txt', weights_only=True)
+model = Model()
+copy = torch.load('weights.pth', weights_only=True)
+model.load_state_dict(copy)
+
+description = [
+    'Постройки',
+    'Лес',
+    'Айсберг',
+    'Гора',
+    'Море',
+    'Улица'
+]
--- server/train_model.py
+++ server/train_model.py
@@ -5,6 +5,7 @@ import random as rd
 from tqdm import tqdm
 import os
 
+
 transforms = tfs_v2.Compose([
     tfs_v2.Resize((150, 150)),
     tfs_v2.ToImage(),
@@ -14,21 +15,89 @@ transforms = tfs_v2.Compose([
 class Model(torch.nn.Module):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.layers = torch.nn.Sequential(
-            torch.nn.Conv2d(3, 20, 3, 1, 1, bias=False),
-            torch.nn.MaxPool2d(3),
-            torch.nn.BatchNorm2d(20),
-            torch.nn.Conv2d(20, 50, 3, 1, 1, bias=True),
-            torch.nn.Conv2d(50, 200, 3, 1, 1, bias=False),
-            torch.nn.MaxPool2d(10),
-            torch.nn.BatchNorm2d(200),
-            torch.nn.Flatten(),
-            torch.nn.Linear(5000, 1000),
+
+        self.layer1 = torch.nn.Sequential(
+            torch.nn.Conv2d(3, 16, 3, 1, 1, bias=False),
+            torch.nn.BatchNorm2d(16),
+            torch.nn.Conv2d(16, 32, 3, 1, 1, bias=False),
+            torch.nn.BatchNorm2d(32),
+            torch.nn.ReLU(True),
+            torch.nn.MaxPool2d(2),
+            torch.nn.Conv2d(32, 48, 3, 1, 1, bias=False),
+            torch.nn.BatchNorm2d(48),
+            torch.nn.Conv2d(48, 64, 3, 1, 1, bias=False),
+            torch.nn.BatchNorm2d(64),
+            torch.nn.ReLU(True),
+            torch.nn.MaxPool2d(2, padding=1)) # 64 38 38
+
+        self.layer2 = torch.nn.Sequential(
+            torch.nn.Conv2d(64, 96, 3, 1, 1, bias=False),
+            torch.nn.BatchNorm2d(96),
+            torch.nn.Conv2d(96, 128, 3, 1, 1, bias=False),
+            torch.nn.BatchNorm2d(128),
+            torch.nn.ReLU(True),
+            torch.nn.MaxPool2d(2),
+            torch.nn.Conv2d(128, 192, 3, 1, 1, bias=False),
+            torch.nn.BatchNorm2d(192),
+            torch.nn.Conv2d(192, 256, 3, 1, 1, bias=False),
+            torch.nn.BatchNorm2d(256),
+            torch.nn.ReLU(True),
+            torch.nn.MaxPool2d(4, padding=1) # 256 5 5
+        )
+        self.layer3 = torch.nn.Sequential(
+            torch.nn.Conv2d(256, 384, 3, 1, 1, bias=False),
+            torch.nn.BatchNorm2d(384),
+            torch.nn.Conv2d(384, 512, 3, 1, 1, bias=False),
+            torch.nn.BatchNorm2d(512),
+            torch.nn.ReLU(True),
+            torch.nn.MaxPool2d(2, padding=1), # 512 3 3
+            torch.nn.Flatten()
+        )
+        self.skip = torch.nn.Sequential(
+            torch.nn.Conv2d(3, 64, 4, 4, 1, bias=False),
+            torch.nn.BatchNorm2d(64)
+        )
+        self.skip2 = torch.nn.Sequential(
+            torch.nn.Conv2d(64, 256, 8, 8, 1, bias=False),
+            torch.nn.BatchNorm2d(256)
+        )
+        self.linears = torch.nn.Sequential(
+            torch.nn.Linear(4608, 1000),
+            torch.nn.BatchNorm1d(1000),
+            torch.nn.ReLU(True),
+            torch.nn.Dropout(0.3),
             torch.nn.Linear(1000, 6)
         )
 
+        self._initialize_weights()
+    
+    def _initialize_weights(self): # функция для инициализации весов
+        for m in self.modules():
+            if isinstance(m, torch.nn.Conv2d):
+                if m in [self.skip[0], self.skip2[0]]:  # Особый случай для skip-connection
+                    torch.nn.init.xavier_uniform_(m.weight, gain=torch.nn.init.calculate_gain('relu'))
+                else:
+                    torch.nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                
+                if m.bias is not None:
+                    torch.nn.init.constant_(m.bias, 0)
+                    
+            elif isinstance(m, torch.nn.BatchNorm2d):
+                torch.nn.init.constant_(m.weight, 1)
+                torch.nn.init.constant_(m.bias, 0)
+                
+            elif isinstance(m, torch.nn.Linear):
+                torch.nn.init.xavier_normal_(m.weight)
+                if m.bias is not None:
+                    torch.nn.init.constant_(m.bias, 0.1)
+
     def forward(self, x):
-        return self.layers(x)
+        out = self.layer1(x)
+        x = self.skip(x) + out
+        out = self.layer2(x)
+        x = self.skip2(x) + out
+        x = self.layer3(x)
+        return self.linears(x)
 
 
 class Dataset(torch.utils.data.Dataset):
@@ -77,18 +146,37 @@ class Dataset(torch.utils.data.Dataset):
 for_train = Dataset()
 for_test = Dataset(False)
 
-train_data = torch.utils.data.DataLoader(for_train, batch_size=10, shuffle=True)
-test_data = torch.utils.data.DataLoader(for_test, batch_size=10, shuffle=True)
+train_data = torch.utils.data.DataLoader(for_train, batch_size=32, shuffle=True)
+test_data = torch.utils.data.DataLoader(for_test, batch_size=32, shuffle=True)
 
 model = Model()
+try:
+  model.load_state_dict(torch.load('weights.pth', weights_only=True))
+except:
+  pass
+model.train()
 
 optimizator = torch.optim.Adam(model.parameters(), weight_decay=0.01)
 loss_func = torch.nn.CrossEntropyLoss()
 
-epochs = 10
+epochs = 30
+acc = []
 
 for e in range(epochs):
-    train_data_tqdm = tqdm(train_data, f'Эпоха: {e}/{epochs}')
+    model.eval()
+    cnt = 0
+    total = 0
+    with torch.no_grad():
+        test_data_tqdm = tqdm(test_data, 'Прогресс:')
+        for x, y in test_data_tqdm:
+            y_pred = model(x)
+            cnt += torch.sum(torch.argmax(torch.softmax(y_pred, dim=1), dim=1) == torch.argmax(y, dim=1)).item()
+            total += y.size(0)
+        acc.append([e, cnt/total])
+        print(f'Accuracy:{cnt/total}')
+
+    model.train()
+    train_data_tqdm = tqdm(train_data, f'Эпоха: {e+1}/{epochs}')
     for x, y in train_data_tqdm:
         y_pred = model(x)
         loss = loss_func(y, y_pred)
@@ -96,3 +184,4 @@ for e in range(epochs):
         optimizator.zero_grad()
         loss.backward()
         optimizator.step()
+    torch.save(model.state_dict(), 'weights.pth') #сохранение весов каждую эпоху
